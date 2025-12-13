2626efeaf481ec017c8c1d9971b10ddf270d26da
endorivium <jihrenberger@gmail.com>
2025-12-13 23:06:03 +0100

    db

--- DB/ExSolutions/ex10/Ex10_Animals.sql
+++ DB/ExSolutions/ex10/Ex10_Animals.sql
@@ -1,4 +1,4 @@
-drop table animal_eats;
+--drop table animal_eats;
 
 create table animal_eats(
 	animal varchar(30),
@@ -19,10 +19,49 @@ insert into animal_eats (animal, eats) values
 	('Wolf','Apple');
 
 
-
 -- Insert the following line to generate a circle
-insert into tier_frisst (animal, eats) values
+insert into animal_eats (animal, eats) values
 	('Fly','Wolf');
 
+-- task 04
+-- a
+with food_chain as (
+select ae.animal, ae.eats, cast(ae.animal +  ' eats ' + ae.eats as varchar(max)) as chain
+from animal_eats as ae
 
+union all
 
+select fc.animal, ae2.eats, cast(chain + ' eats ' + ae2.eats as varchar(max))
+from food_chain as fc, animal_eats as ae2
+where fc.eats = ae2.animal
+)
+
+select * from food_chain;
+
+-- b
+with cycle_food_chain as (
+select ae.animal, ae.eats, cast(ae.animal +  ' eats ' + ae.eats as varchar(max)) as chain
+from animal_eats as ae
+
+union all
+
+select cfc.animal, ae2.eats, cast(chain + ' eats ' + ae2.eats as varchar(max))
+from cycle_food_chain as cfc, animal_eats as ae2
+where cfc.eats = ae2.animal and chain not like '%'+ae2.eats+'%'
+)
+
+select * from cycle_food_chain;
+
+-- c
+with indirect_food_chain as (
+select ae.animal, ae.eats as eats, cast('' as varchar(max)) as indirectly_eats, cast(ae.animal +  ' eats ' + ae.eats as varchar(max)) as chain
+from animal_eats as ae
+
+union all
+
+select ifc.animal, ae2.eats, cast(ae2.eats as varchar(max)) as indirectly_eats, cast(chain + ' eats ' + ae2.eats as varchar(max))
+from indirect_food_chain as ifc, animal_eats as ae2
+where ifc.eats = ae2.animal and chain not like '%'+ae2.eats+'%'
+)
+
+select * from indirect_food_chain
